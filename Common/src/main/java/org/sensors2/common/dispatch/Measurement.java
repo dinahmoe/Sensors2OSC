42516447e62f88c83d212f61422c37af713c381e
Alessandro Saccoia <alessandro@nvstudio.de>
2015-08-28 21:53:14 +0200

    Added common as part of the repo

---
+++ Common/src/main/java/org/sensors2/common/dispatch/Measurement.java
@@ -0,0 +1,59 @@
+package org.sensors2.common.dispatch;
+
+import android.net.wifi.ScanResult;
+import android.view.MotionEvent;
+
+/**
+ * Created by thomas on 03.11.14.
+ */
+public class Measurement {
+	private final int sensorType;
+	private final float[] values;
+	private final String name;
+	private final MeasurementType type;
+    private long timestamp;
+
+	public Measurement(int sensorType, long timestamp, float[] values) {
+		this.sensorType = sensorType;
+		this.values = values;
+        this.timestamp = timestamp;
+		this.name = "";
+		this.type = MeasurementType.Sensor;
+	}
+
+	public Measurement(ScanResult scanResult) {
+		this.sensorType = 0;
+        this.name = scanResult.SSID.replaceAll(" ", "-"); // the receiver can't have spaces
+        this.values = new float[2];
+        this.values[0] = scanResult.level;
+		this.values[1] = scanResult.frequency;
+		this.type = MeasurementType.Wifi;
+	}
+
+	public Measurement(MotionEvent motionEvent){
+		this.sensorType = 0;
+		this.name = "";
+		this.values = new float[2];
+		this.values[0] = motionEvent.getX();
+		this.values[1] = motionEvent.getY();
+		this.type = MeasurementType.Touch;
+	}
+
+	public float[] getValues() {
+		return values;
+	}
+
+	public int getSensorType() {
+		return sensorType;
+	}
+
+	public MeasurementType getType() {
+		return type;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+    public long getTimestamp() { return timestamp; }
+}
